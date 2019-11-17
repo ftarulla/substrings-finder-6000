37022492c461709023529e8c9444eaf4ecdf6c74
Franciscello <ftarulla@gmail.com>
2019-11-17 09:33:37 -0300

    Add link reference and comment out refresh button

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
     const router = {
       current: () => this.state.path,
       navigate: (path, state) => {
+        // http://es6-features.org/#SpreadOperator
         this.setState({ ...state, path: path })
       }
     }
